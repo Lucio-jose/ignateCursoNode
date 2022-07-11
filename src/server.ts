dd2ec46f93dec73e534791d28cfe8005a0aeb6d9
Lucio-jose <luciocanganjo2@gmail.com>
2022-07-11 13:29:17 +0100

    Debug

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,15 @@ import  express  from "express";
 
 const app = express();
 
+app.use(express.json())
+
+app.post('/User',(Request, Response)=>{
+    
+    const {nome}=Request.body;
+    
+    return Response.json({nome})
+})
+
 app.get('/',(Request, Response)=>{
     return Response.status(200).json({message:"Ola Mundo2"})
 })
