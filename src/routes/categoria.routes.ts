582713f9bd6ae8671107443f2fd5d63850273c54
Lucio-jose <luciocanganjo2@gmail.com>
2022-07-26 18:57:00 +0100

    uploads

--- src/routes/categoria.routes.ts
+++ src/routes/categoria.routes.ts
@@ -1,8 +1,19 @@
+import multer from "multer";
 import { Router, Request, Response} from "express";
 import { createCategoryController } from "../modules/cars/useCase/createCategory";
 import { listCategoryController } from "../modules/cars/useCase/ListCategory";
 
 const categoriaRoute = Router();
+
+const upload = multer({
+    dest: './temp'
+})
+
+categoriaRoute.post("/import", upload.single("file"), (req: Request, res: Response)=>{
+    const{file}=req;
+    console.log(file)
+    return res.status(201).send()
+ })
 
 categoriaRoute.post("/",(req: Request, res: Response)=>{
    return createCategoryController.handle(req, res)
